01033a59852d7d3392e17ed652b559e1311a8f63
Michael <micah.workspace@gmail.com>
2025-06-19 16:12:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,12 +18,12 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || 'An Engineer', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://godofmachining.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Michael, Blog', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/https://www.canva.com/design/DAGqryiSzEM/AwyX7EDiSRcQHfTfSvRKHw/view?utm_content=DAGqryiSzEM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h74b1c5e98d', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || '', // 备案查询链接，如果用了萌备等备案请在这里填写
 
   // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
 
   // 其它复杂配置
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Welcome to God Of Machining',
+    '',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
